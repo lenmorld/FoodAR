c25dbdec0725b67bfd9da942323109625258bcac
Lenmor Ld <lenmorld@gmail.com>
2018-03-30 01:46:22 -0400

    render food item name as AR content

--- app/ar-web/ar-web-module.js
+++ app/ar-web/ar-web-module.js
@@ -237,7 +237,9 @@ var ArWebModule = function () {
 
                 // var push = new THREE.Vector3(0, 0, -1.0);
 
-                var push = new THREE.Vector3(-0.5, 0, -0.5);
+                var push = new THREE.Vector3(0, 0, 0);
+                // var push = new THREE.Vector3(-0.5, 0, -0.5);
+
                 push.transformDirection(dirMtx);
                 pos.addScaledVector(push, 0.125);
 
