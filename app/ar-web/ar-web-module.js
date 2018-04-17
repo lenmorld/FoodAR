02e756d470a19f78c94791547c99304ca75d2c20
Lenmor Ld <lenmorld@gmail.com>
2018-04-16 22:37:13 -0400

    2d text

--- app/ar-web/ar-web-module.js
+++ app/ar-web/ar-web-module.js
@@ -340,21 +340,21 @@ var ArWebModule = function () {
         try {
             Utils.debug(text + " " + yPos);
 
-
-            var context1 = canvas.getContext('2d');
+            var canvas1 = document.createElement('canvas');
+            var context1 = canvas1.getContext('2d');
             context1.font = "Bold 40px Arial";
             context1.fillStyle = "rgba(255,0,0,0.95)";
             context1.fillText(text, 0, yPos);
 
             // canvas contents will be used for a texture
-            var texture1 = new THREE.Texture(canvas);
+            var texture1 = new THREE.Texture(canvas1);
             texture1.needsUpdate = true;
 
             var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
             material1.transparent = true;
 
             var mesh1 = new THREE.Mesh(
-                new THREE.PlaneGeometry(canvas.width, canvas.height),
+                new THREE.PlaneGeometry(canvas1.width, canvas1.height),
                 material1
             );
             mesh1.position.set(0,0,0);
